8909da88c15e7ac88a366bd12f9602af9b389593
hyunmindev <jung@hyunmin.dev>
2023-04-01 20:57:31 +0900

    sitemap 설정 최신화

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,18 @@
 module.exports = {
   siteUrl: 'https://blog.hyunmin.dev',
   generateRobotsTxt: true,
+  transform: async (config, path) => {
+    if (path.includes('posts')) {
+      return {
+        loc: path,
+        changefreq: 'monthly',
+        priority: 1,
+      };
+    }
+    return {
+      loc: path,
+      changefreq: 'monthly',
+      priority: 0.8,
+    };
+  },
 };
