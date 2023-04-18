87662c61c0b1a5fdc9d87ca8725bf0766f76e236
hyunmindev <jung@hyunmin.dev>
2023-04-18 22:44:39 +0900

    루트 OG 이미지 제작

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -4,15 +4,20 @@ export const config = {
   runtime: 'edge',
 };
 
+const fontURL = new URL('../../../assets/fonts/fira.ttf', import.meta.url);
+
 export async function GET() {
+  const res = await fetch(fontURL);
+  const fontData = await res.arrayBuffer();
+
   return new ImageResponse(
     (
       <div
         style={{
           alignItems: 'center',
-          backgroundColor: 'white',
+          backgroundColor: '#262729',
           backgroundImage:
-            'radial-gradient(circle at 25px 25px, lightgray 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)',
+            'radial-gradient(circle at 25px 25px, #555 2%, transparent 0%), radial-gradient(circle at 75px 75px, #555 2%, transparent 0%)',
           backgroundSize: '100px 100px',
           display: 'flex',
           flexDirection: 'column',
@@ -25,36 +30,41 @@ export async function GET() {
       >
         <div
           style={{
-            alignItems: 'center',
             display: 'flex',
-            justifyContent: 'center',
+            flexDirection: 'column',
           }}
         >
-          <svg
-            fill="black"
-            height={80}
-            style={{ margin: '0 75px' }}
-            viewBox="0 0 75 65"
+          <h1
+            style={{
+              color: '#e6e7ec',
+              fontSize: 136,
+              fontWeight: 500,
+              marginBottom: -24,
+              textShadow: '0 6px #6598d2, 6px 0 #e06d3d, 0 -6px #c5ab48',
+            }}
           >
-            <path d="M37.59.25l36.95 64H.64l36.95-64z" />
-          </svg>
-        </div>
-        <div
-          style={{
-            color: 'black',
-            display: 'flex',
-            fontSize: 40,
-            fontStyle: 'normal',
-            lineHeight: 1.8,
-            marginTop: 30,
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          <b>Hyunmin Jung</b>
+            Hyunmin Jung
+          </h1>
+          <p
+            style={{
+              color: '#87959d',
+              fontSize: 28,
+              fontWeight: 400,
+            }}
+          >
+            front-end developer
+          </p>
         </div>
       </div>
     ),
     {
+      fonts: [
+        {
+          data: fontData,
+          name: 'fira',
+          style: 'normal',
+        },
+      ],
       height: 600,
       width: 1200,
     }
