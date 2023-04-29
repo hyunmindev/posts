8890e0ac97264e8bb683ba4d240e245031610916
hyunmindev <jung@hyunmin.dev>
2023-04-29 18:28:30 +0900

    커스텀 상호작용 패널 제거

--- src/app/api/likes/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function PUT(request: NextRequest) {
-  const res = await request.json();
-  return NextResponse.json(res);
-}
