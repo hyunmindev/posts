38d689a28c6f14fe1c2e4baaaf95358fafc94f24
hyunmindev <jung@hyunmin.dev>
2023-05-01 11:33:11 +0900

    server-sitemap 리버트

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,18 +2,9 @@
 module.exports = {
   siteUrl: 'https://blog.hyunmin.dev',
   generateRobotsTxt: true,
-  transform: async (config, path) => {
-    if (path.includes('posts')) {
-      return {
-        loc: path,
-        changefreq: 'monthly',
-        priority: 1,
-      };
-    }
-    return {
-      loc: path,
-      changefreq: 'monthly',
-      priority: 0.8,
-    };
+  generateIndexSitemap: false,
+  exclude: ['/api/*', '/favicon.ico', '/posts/*', '/server-sitemap.xml'],
+  robotsTxtOptions: {
+    additionalSitemaps: ['https://blog.hyunmin.dev/server-sitemap.xml'],
   },
 };
