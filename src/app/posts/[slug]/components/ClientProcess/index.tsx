e2947b61143c0e6301c89c86360784ea281c3454
hyunmindev <jung@hyunmin.dev>
2023-05-02 20:41:09 +0900

    코드 카피 버튼 추가

--- src/app/posts/[slug]/components/ClientProcess/index.tsx
+++ src/app/posts/[slug]/components/ClientProcess/index.tsx
@@ -21,18 +21,23 @@ function ClientProcess({ children, slug }: PropsWithChildren<Props>) {
 
   /* 복사 버튼 추가 */
   useEffect(() => {
-    const button = document.createElement('button');
-    button.onclick = function handleClick() {
-      // @ts-ignore
-      const code = this!.parentElement.children[0].innerText;
-      navigator.clipboard.writeText(code);
-    };
-    button.setAttribute('aria-label', 'copy code');
-    button.classList.add('copy');
-    button.innerHTML =
-      '<svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 96 960 960" width="18"><path d="M180 975q-24 0-42-18t-18-42V312h60v603h474v60H180Zm120-120q-24 0-42-18t-18-42V235q0-24 18-42t42-18h440q24 0 42 18t18 42v560q0 24-18 42t-42 18H300Zm0-60h440V235H300v560Zm0 0V235v560Z"/></svg>';
     const pres = document.querySelectorAll('pre');
-    pres.forEach((pre) => pre.appendChild(button));
+    pres.forEach((pre) => {
+      const button = document.createElement('button');
+      button.addEventListener('click', function handleClick() {
+        const code = pre.querySelector('code');
+        button.classList.add('copied');
+        navigator.clipboard.writeText(code!.innerText ?? '');
+      });
+      pre.addEventListener('mouseleave', () => {
+        button.classList.remove('copied');
+      });
+      button.innerHTML =
+        '<svg viewBox="0 96 960 960"><path d="M180 975q-24 0-42-18t-18-42V312h60v603h474v60H180Zm120-120q-24 0-42-18t-18-42V235q0-24 18-42t42-18h440q24 0 42 18t18 42v560q0 24-18 42t-42 18H300Zm0-60h440V235H300v560Zm0 0V235v560Z"/></svg>';
+      button.setAttribute('aria-label', 'copy code');
+      button.classList.add('copy');
+      pre.appendChild(button);
+    });
   }, []);
 
   return <>{children}</>;
