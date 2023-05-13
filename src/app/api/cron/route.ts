5a26ffe0938424afbaf4e1d559ea7b6c16323afc
hyunmindev <jung@hyunmin.dev>
2023-05-13 22:53:28 +0900

    vercel cron 제거

--- src/app/api/cron/route.ts
+++
@@ -1,7 +0,0 @@
-// import { execSync } from 'child_process';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  // const data = execSync('npx vercel --prod --token=Tmqec4prkz76FeudyBkT3NPA');
-  return NextResponse.json({});
-}
